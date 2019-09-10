2e39dee8e7fd85068ee7ce6e7c8a56189cf140a8
Marcelo T. Cortes <marceloteixeiracortes@gmail.com>
2019-09-10 14:03:04 -0300

    Pilha

---
+++ Pilha/src/Conversor.java
@@ -0,0 +1,25 @@
+
+public class Conversor {
+	public static void main(String[] args) {
+		int numero = 172;
+		Pilha p = new Pilha();
+		
+		int resto;
+		
+		// fase 1: empilhamento dos restos
+		while (numero != 0 ){
+			resto = numero % 2; // pego o resto da divisão deste númeor por 2 (vale o ou 1)
+			p.push(resto); //armazeno na pilha 
+			numero = numero /2; // gero um novo número, resultado da divisão dele por 2
+		}
+		
+		//fase 2: exibição dos restos
+		while(!p.isEmpty()){
+			resto = p.pop();	//remove da pilha (sempre o cara que esta no topo)
+			System.out.println(resto);
+		}
+		
+		System.out.println("Fim do Programa");
+	}
+
+} 
---
+++ Pilha/src/Pilha.java
@@ -0,0 +1,34 @@
+
+public class Pilha {
+	private int valores[];
+	private int topo;
+	
+	public Pilha() {
+		valores = new int[10];
+		topo	= -1; 
+		 
+	}
+	
+	public void push(int elemento){
+		topo = topo + 1;
+		valores[topo] = elemento;
+	}
+	
+	public boolean isEmpty(){
+		return (topo == -1);
+		
+	}
+	
+	public boolean isFull(){
+		return (topo == 9); //tamanho menos 1
+		
+	}
+	
+	public int pop(){
+		int elem = valores[topo]; 
+		topo--;
+		return elem;
+		
+	}
+}
+ 
